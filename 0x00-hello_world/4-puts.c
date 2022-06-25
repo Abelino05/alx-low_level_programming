5bc249a97fcb18be455b17a108733a06ee737fe2
Abelino05 <pakpaliabel@gmail.com>
2022-06-25 14:57:39 -0700

    PUTS

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,8 @@
-/*
- * This function print a text an return 0 at the end
- */
-
 #include <stdio.h>
 
 int main(void)
 {
+/*This function print a text return 0 at the end*/
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
