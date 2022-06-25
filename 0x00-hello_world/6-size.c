db15441b20d5d838b209beedf18c944d07234c7f
Abelino05 <pakpaliabel@gmail.com>
2022-06-25 16:17:20 -0700

    Size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+/**
+ * main - Print sizes of variables
+ *
+ * Return: 0 at the end
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	char my_char;
+	int my_int;
+	long my_long_int;
+	long my_long_long_int;
+	float my_float;
+	printf("Size of a char : %c byte(s)", sizeof(my_char));
+	printf("Size of an int : %d byte(s)", sizeof(my_int));
+	printf("Size of a long int : %ld byte(s)", sizeof(my_long_int));
+	printf("Size of a long long int : %ld byte(s)", (unsigned long)sizeof(my_long_long_int));
+	printf("Size of a float : %f byte(s)", sizeof(my_float));
+}
+
