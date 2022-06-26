db2ca4af7d0c5993784f6ff83a6c13934b307586
Abelino05 <pakpaliabel@gmail.com>
2022-06-26 04:32:23 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,5 @@
 /**
- * main - Prints excatly and that piece of art is useful\
- * \" - Dora Korpar, 2015-10-19
+ * main - Prints excatly and that piece of art is usefuland that piece of art is useful - Dora Korpar, 2015-10-19
  *
  * Return: 0 at the end
  */
@@ -10,7 +9,7 @@
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-	return (0);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
 
