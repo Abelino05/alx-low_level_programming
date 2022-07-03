486db4cc6ca10c3bd23354d4c5db7a76de564213
Abelino05 <pakpaliabel@gmail.com>
2022-07-03 08:12:51 -0700

    Print alphabet with exception

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints alphabet in lowercase except e and q
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'q' && c != 'e')
+		{
+			putchar(c);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
