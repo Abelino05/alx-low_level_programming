1913233c91860998d8d9888d039e523dde3260b3
Abelino05 <pakpaliabel@gmail.com>
2022-07-03 07:30:30 -0700

    Print numbers with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Print numbers using putchar
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i+'0');/*Convert in char*/
+	}
+	putchar('\n');
+	return (0);
+}
