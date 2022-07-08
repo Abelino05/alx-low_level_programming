153490e7d3b0af2ae06c725b15114aa8219ec16e
Abelino05 <pakpaliabel@gmail.com>
2022-07-07 19:16:21 -0700

    Print lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - Print lines
+ * @n: number of line
+ * Return: 0
+ */
+
+void print_line(int n)
+{
+	int j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (j = 0; j <= n; j++)
+		{
+			_putchar (95);
+		}
+	}
+	_putchar('\n');
+}
