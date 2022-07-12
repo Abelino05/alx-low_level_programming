6648607cd5b2494ba59d377edf57d29bdb79e812
Abelino05 <pakpaliabel@gmail.com>
2022-07-12 10:49:09 -0700

    Prints any other character

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+#include <string.h>
+
+/**
+ * rev_string - Prints the string in reverse
+ * @s: The string to print
+ */
+
+void rev_string(char *s)
+{
+	int i;
+
+}
---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+#include <string.h>
+
+/**puts - Prints any other character
+ * @str: String to print
+ */
+
+void puts2(char *str)
+{
+	int len;
+	int i;
+	for (i = 0; i < len; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
