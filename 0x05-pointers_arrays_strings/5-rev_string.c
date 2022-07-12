75d661b8bd632ce7e8a39b6cbc51c44b0425f006
Abelino05 <pakpaliabel@gmail.com>
2022-07-12 11:35:49 -0700

    Reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,5 +10,24 @@
 void rev_string(char *s)
 {
 	int i;
+	int j;
+	int k;
+	int temp;
 
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	k = 0;
+	j = i - 1;
+
+	while (k < j)
+	{
+		temp = s[k];
+		s[k] = s[j];
+		s[j] = temp;
+		k++;
+		j--;
+	}
 }
