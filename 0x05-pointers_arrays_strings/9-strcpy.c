7456934c33d96ccc435b435b9c7b0bab019013cc
Abelino05 <pakpaliabel@gmail.com>
2022-07-12 14:20:20 -0700

    Copy string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcpy - copy the string pointed to
+ *@dest: first parameter
+ *@src: second parameter
+ *Return: the pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	char *start = dest;
+	
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (start);
+}
