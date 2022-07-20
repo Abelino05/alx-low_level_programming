d9d41a6e490ce263447c09b855ddffb1b3c8f686
Abelino05 <pakpaliabel@gmail.com>
2022-07-20 11:22:07 -0700

    Print reversion fixed

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -9,13 +9,8 @@
 
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
-	{
-		return (0);
-	}
-	else
-	{
-		_print_rev_recursion(s + 1);
+	if (*s != '\0')
+	{	_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
 
