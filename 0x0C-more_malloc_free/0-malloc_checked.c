13fb3ee67b087234f0210e3c3e29bfc172928c93
Abelino05 <pakpaliabel@gmail.com>
2022-07-28 17:10:32 -0700

    Strings concatenate

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -5,16 +5,17 @@
 /**
  * malloc_checked - Function that allocate memory using malloc
  * @b: Size to allocate
- * 
  * Return: Apointer to the allocated memory
  */
 
 void *malloc_checked(unsigned int b)
 {
 	int *mem;
-	
+
 	mem = malloc(b);
-	if (mem = NULL)
-		exit (98);
+
+	if (mem == NULL)
+		exit(98);
+
 	return (mem);
 }
