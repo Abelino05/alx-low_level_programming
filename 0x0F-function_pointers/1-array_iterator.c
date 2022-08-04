c48ff7908046d61518ea3130924e733e17c5c99e
Abelino05 <pakpaliabel@gmail.com>
2022-08-04 00:33:42 +0000

    Iterator fixed

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,7 +1,7 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator - Function that executes a function given 
+ * array_iterator - Function that executes a function given
  * as a parameter on each element of an array.
  * @array: array of intege
  * @size: size of array
